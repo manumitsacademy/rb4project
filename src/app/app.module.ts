ed8f348b29ad4d0a434c60cc81fc24077185f59a
Praveen-at-289810389200 <manumits.info@gmail.com>
2020-07-12 08:35:50 +0530

    'banking'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainAreaComponent } from './main-area/main-area.component';
+import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainAreaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/main-area/main-area.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main-area',
+  templateUrl: './main-area.component.html',
+  styleUrls: ['./main-area.component.css']
+})
+export class MainAreaComponent{
+
+  trainer = "praveen";
+  newTrainer="";
+  addTrainerFlag = false;
+  trainers:string[] | undefined;
+  choice='bank';
+  selectedAccountType=null;
+  selectedServiceType=null;
+  addTrainer(){
+
+    this.trainers === undefined ? this.trainers = []:'';
+    this.addTrainerFlag = true;
+    setTimeout(()=>{
+      this.trainers.push(this.newTrainer);
+      this.newTrainer = '';
+      this.addTrainerFlag = false;
+    },3000)
+    
+  }
+  deleteTrainer(index){
+    this.trainers.splice(index,1);
+    if(this.trainers.length===0){
+      this.trainers=undefined;
+    }
+  }
+}
